36625139477e1472b27f8a617938ed0f880ced33
Sachin Porwal <sachin.porwal12@gmail.com>
2026-01-11 13:35:37 +0000

    Fix ALB subnet_ids wiring

--- terraform/main.tf
+++ terraform/main.tf
@@ -27,7 +27,7 @@ module "ec2" {
 module "alb" {
   source     = "./modules/alb"
   vpc_id     = module.vpc.vpc_id
-  subnet_id = module.vpc.public_subnets
+  subnet_ids = module.vpc.public_subnets
   instance_id = module.ec2.instance_id
 }
 
